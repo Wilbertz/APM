6a2caff848b38b47a172ddab7e73515387162339
Harald Wilbertz <harald.wilbertz@gmx.de>
2021-02-28 13:15:15 +0100

    Filtering implemented.

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -11,7 +11,17 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin: number = 2; 
     showImage: boolean = false;
-    listFilter: string = 'cart';
+
+    private _listFilter: string;
+    get listFilter(): string {
+      return this._listFilter;
+    }
+    set listFilter(value: string) {
+      this._listFilter = value;
+      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter): this.products;
+    }
+
+    filteredProducts: IProduct[];
     products: IProduct[] = 
     [
       {
@@ -66,6 +76,11 @@ export class ProductListComponent implements OnInit{
       }
     ];
 
+    constructor() {
+      this.filteredProducts = this.products;
+      this.listFilter = 'cart';
+    }
+
     ngOnInit(): void {
       console.log('in OnInit');
     }
@@ -73,4 +88,11 @@ export class ProductListComponent implements OnInit{
     toggleImage(): void {
       this.showImage = !this.showImage;
     }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter(
+          (product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
 }
